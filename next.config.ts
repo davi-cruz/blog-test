b8728c6710f1b0be0562a88b453c614866e1a252
davicruz <davicruz@google.com>
2024-12-25 22:33:30 +0000

    adjust environments

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,15 @@ const nextConfig = (phase: string, { defaultConfig }: any) => {
   output: export,
   if (phase === 'phase-development-server') {
     return {
-      // Development-only config options
-      basePath: '/blog-test', 
+      // Development-only config options 
     }
-  } 
+  } else {
+    // Production config options (including 'phase-production-build' and 'phase-export')
+    return {
+      basePath: '/blog-test', 
+      // ... other production configurations ...
+    }
+  }
 }
 
 export default nextConfig;
